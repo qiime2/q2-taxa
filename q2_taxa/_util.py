64c6eb7464acbcdcd2143b7988b440a41dd71157
Greg Caporaso <gregcaporaso@gmail.com>
2016-10-06 18:36:02 -0700

    ENH: adds collapse method (#6)
    fixes #4

--- q2_taxa/_util.py
+++ q2_taxa/_util.py
@@ -1,5 +1,9 @@
-def _extract_to_level(taxonomy, table, max_level=None):
-    # Assemble the taxonomy data
+
+import biom
+import pandas as pd
+
+
+def _assemble_taxonomy_data(taxonomy, table):
     taxa = {}
     max_obs_lvl = 0
     for k, v in taxonomy.iteritems():
@@ -7,6 +11,23 @@ def _extract_to_level(taxonomy, table, max_level=None):
         max_obs_lvl = len(levels) if len(levels) > max_obs_lvl else max_obs_lvl
         taxa[k] = {'taxonomy': levels}
     table.add_metadata(taxa, axis='observation')
+
+    return table, max_obs_lvl
+
+
+def _collapse_table(table, level, max_observed_level):
+    def bin_f(id_, x):
+        if len(x['taxonomy']) < max_observed_level:
+            padding = ['__'] * (max_observed_level - len(x['taxonomy']))
+            x['taxonomy'].extend(padding)
+        return ';'.join(x['taxonomy'][:level])
+    return table.collapse(bin_f, norm=False, min_group_size=1,
+                          axis='observation')
+
+
+def _extract_to_level(taxonomy, table, max_level=None):
+    # Assemble the taxonomy data
+    table, max_obs_lvl = _assemble_taxonomy_data(taxonomy, table)
 
     if max_level is None:
         max_level = max_obs_lvl
@@ -19,13 +40,23 @@ def _extract_to_level(taxonomy, table, max_level=None):
     collapsed_tables = []
     # Collapse table at specified level
     for level in range(1, max_level + 1):
-        def bin_f(id_, x):
-            if len(x['taxonomy']) < max_obs_lvl:
-                padding = ['__'] * (max_obs_lvl - len(x['taxonomy']))
-                x['taxonomy'].extend(padding)
-            return ';'.join(x['taxonomy'][:level])
-        collapsed_tables.append(table.collapse(bin_f, norm=False,
-                                               min_group_size=1,
-                                               axis='observation'))
+        collapsed_table = _collapse_table(table, level, max_obs_lvl)
+        collapsed_tables.append(collapsed_table)
 
     return collapsed_tables
+
+
+def collapse(table: biom.Table, taxonomy: pd.Series, level: int) -> biom.Table:
+    if level < 1:
+        raise ValueError('Requested level of %d is too low. Must be greater '
+                         'than or equal to 1.' % level)
+
+    # Assemble the taxonomy data
+    table, max_observed_level = _assemble_taxonomy_data(taxonomy, table)
+
+    if level > max_observed_level:
+        raise ValueError('Requested level of %d is larger than the maximum '
+                         'level available in taxonomy data (%d).' %
+                         (level, max_observed_level))
+
+    return _collapse_table(table, level, max_observed_level)
