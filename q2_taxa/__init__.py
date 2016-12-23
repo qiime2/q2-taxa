80f7712623152635bb63aacda3455ae885324888
Matthew Dillon <matthewrdillon@gmail.com>
2016-12-23 09:09:44 -0700

    ENH: new date-based versioning; `qiime` -> `qiime2` package rename
    New date-based versioning scheme is:
    
    - YYYY.M.patch.dev0 for development version (e.g. 2017.2.0.dev0, 2017.2.1.dev0, 2018.10.0.dev0)
    - YYYY.M.patch for release version (e.g. 2017.2.0, 2017.2.1, 2018.10.0)
    
    Plugins/interfaces are recommended to specify `qiime2 == YYYY.M.*` in setup.py's `install_requires` to obtain patch releases for a given train release.
    
    The new versioning scheme is PEP 440 compliant.

--- q2_taxa/__init__.py
+++ q2_taxa/__init__.py
@@ -1,9 +1,12 @@
 # ----------------------------------------------------------------------------
-# Copyright (c) 2016--, QIIME development team.
+# Copyright (c) 2016-2017, QIIME 2 development team.
 #
 # Distributed under the terms of the Modified BSD License.
 #
 # The full license is in the file LICENSE, distributed with this software.
 # ----------------------------------------------------------------------------
 
-__version__ = "0.0.7.dev0"
+import pkg_resources
+
+
+__version__ = pkg_resources.get_distribution('q2-taxa').version
--- q2_taxa/plugin_setup.py
+++ q2_taxa/plugin_setup.py
@@ -1,12 +1,12 @@
 # ----------------------------------------------------------------------------
-# Copyright (c) 2016--, QIIME development team.
+# Copyright (c) 2016-2017, QIIME 2 development team.
 #
 # Distributed under the terms of the Modified BSD License.
 #
 # The full license is in the file LICENSE, distributed with this software.
 # ----------------------------------------------------------------------------
 
-import qiime.plugin
+import qiime2.plugin
 
 import q2_taxa
 
@@ -18,7 +18,7 @@ from ._taxa_visualizer import barplot, tabulate
 from ._util import collapse
 
 
-plugin = qiime.plugin.Plugin(
+plugin = qiime2.plugin.Plugin(
     name='taxa',
     version=q2_taxa.__version__,
     website='https://github.com/qiime2/q2-taxa',
@@ -33,7 +33,7 @@ plugin.methods.register_function(
         'taxonomy': FeatureData[Taxonomy],
         'table': FeatureTable[Frequency]
     },
-    parameters={'level': qiime.plugin.Int},
+    parameters={'level': qiime2.plugin.Int},
     outputs=[('collapsed_table', FeatureTable[Frequency])],
     name='Collapse features by their taxonomy at the specified level',
     description='Collapse groups of features that have the same taxonomic '
@@ -47,7 +47,7 @@ plugin.visualizers.register_function(
         'taxonomy': FeatureData[Taxonomy],
         'table': FeatureTable[Frequency]
     },
-    parameters={'metadata': qiime.plugin.Metadata},
+    parameters={'metadata': qiime2.plugin.Metadata},
     name='Visualize taxonomy with an interactive bar plot',
     description='This visualizer produces an interactive barplot visualization'
                 ' of taxonomies. Interactive features include multi-level '
