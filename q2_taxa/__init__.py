1a53a2a62e63e43bd17cc135907fffb845075518
Max von Hippel <maxvonhippel1996@gmail.com>
2017-07-10 13:26:49 -0700

    ENH: Remove tabulate in favor of metadata tabulate

--- q2_taxa/__init__.py
+++ q2_taxa/__init__.py
@@ -7,10 +7,10 @@
 # ----------------------------------------------------------------------------
 
 from ._method import collapse
-from ._visualizer import barplot, tabulate
+from ._visualizer import barplot
 from ._version import get_versions
 
 __version__ = get_versions()['version']
 del get_versions
 
-__all__ = ['barplot', 'tabulate', 'collapse']
+__all__ = ['barplot', 'collapse']
--- q2_taxa/tests/test_visualizer.py
+++ q2_taxa/tests/test_visualizer.py
@@ -13,7 +13,7 @@ import unittest
 import pandas as pd
 import qiime2
 
-from q2_taxa import barplot, tabulate
+from q2_taxa import barplot
 
 
 class BarplotTests(unittest.TestCase):
@@ -34,17 +34,3 @@ class BarplotTests(unittest.TestCase):
             self.assertTrue(os.path.exists(index_fp))
             self.assertTrue("src='lvl-1.jsonp?callback=load_data'" in
                             open(index_fp).read())
-
-
-class TabulateTests(unittest.TestCase):
-
-    def test_tabulate(self):
-        taxonomy = pd.Series(['a; b; c', 'a; b; d'],
-                             index=['feat1', 'feat2'])
-
-        with tempfile.TemporaryDirectory() as output_dir:
-            tabulate(output_dir, taxonomy)
-            index_fp = os.path.join(output_dir, 'index.html')
-            self.assertTrue(os.path.exists(index_fp))
-            self.assertTrue("feat1" in open(index_fp).read())
-            self.assertTrue('a; b; c' in open(index_fp).read())
