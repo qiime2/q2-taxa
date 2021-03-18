06b2389b14ccab90cb0d5024560d3057c190c6ee
FranckLejzerowicz <flejzerowicz@ucsd.edu>
2021-03-18 11:19:54 -0700

    add a  function to infer the ranks, e.g. works for greengenes and other good DBs

--- q2_taxa/_util.py
+++ q2_taxa/_util.py
@@ -11,6 +11,21 @@ def _get_max_level(taxonomy):
     return taxonomy.apply(lambda x: len(x.split(';'))).max()
 
 
+def _get_padding_ranks(taxonomy):
+    # get the maximum number of ranks
+    max_obs_lvl = _get_max_level(taxonomy)
+    # split the taxonomy
+    taxonomy_split = taxonomy.str.split('; ', expand=True)
+    # reduce ranks to strings before (and with) the first underscore(s)
+    poss_ranks = taxonomy_split.apply(
+        lambda x: x.str.extract('(.*?_+)', expand=True)[0], axis=1)
+    # if strings found for every rank and same across taxa (None are ignored)
+    if not poss_ranks.isna().all().sum() and poss_ranks.nunique().max() == 1:
+        # return the list of ranks for padding
+        return list(poss_ranks[~poss_ranks.isna().any(1)].values[0])
+    # otherwise return None, which will not leave with blank padding
+
+
 def _collapse_table(table, taxonomy, level, max_observed_level):
     table_ids = set(table.columns)
     taxonomy_ids = set(taxonomy.index)
@@ -21,10 +36,18 @@ def _collapse_table(table, taxonomy, level, max_observed_level):
 
     table = table.copy()
 
+    padding_ranks = _get_padding_ranks(taxonomy)
+
     def _collapse(tax):
         tax = [x.strip() for x in tax.split(';')]
+
         if len(tax) < max_observed_level:
-            padding = ['__'] * (max_observed_level - len(tax))
+            if padding_ranks:
+                padding = [padding_ranks[x] for x in range(len(tax), max_observed_level)]
+            else:
+                # I suggest this situation gets fixed for a collapsing desire
+                # (probably the taxa could be discarded?)
+                padding = ['__'] * (max_observed_level - len(tax))
             tax.extend(padding)
         return ';'.join(tax[:level])
 
