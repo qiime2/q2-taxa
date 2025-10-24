f7080c3b108637d29de7c6d85b8699591655065c
Colin Wood <woodcolin38@gmail.com>
2025-10-24 10:03:49 -0700

    new color schemes

--- q2_taxa/_barplot_visualizer/src/classes/colors.svelte.ts
+++ q2_taxa/_barplot_visualizer/src/classes/colors.svelte.ts
@@ -14,7 +14,7 @@ export class Colors {
 
     constructor() {
         this.colorSchemes = new Map();
-        this.colorScheme = $state("before-dawn");
+        this.colorScheme = $state("rainbow");
         this.schemeIndex = 0;
         this.colorLevel = 0;
         this.customColors = $state(new SvelteMap());
