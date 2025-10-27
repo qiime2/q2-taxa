b463ba8789036b942d8c79e043cb2609aa53ed4b
Colin Wood <woodcolin38@gmail.com>
2025-10-27 14:47:01 -0700

    truncate the correct elements

--- q2_taxa/_barplot_visualizer/src/lib/HoveredSlice.svelte
+++ q2_taxa/_barplot_visualizer/src/lib/HoveredSlice.svelte
@@ -12,21 +12,21 @@
 <div
     class="flex w-[42rem] h-[4.8rem] items-center bg-gray-100 rounded-lg p-[1rem] ml-[2rem] mt-[1rem]"
 >
-    <div class="flex flex-row items-center w-[16rem] truncate">
+    <div class="flex flex-row items-center w-[16rem]">
         <p class="font-bold text-sm mr-[1rem]">Sample Lables</p>
         <div class="flex flex-col">
             {#if sampleManager.hoveredSlice != null}
                 {#each sampleManager.sampleControls.getSampleLabels(sampleManager.hoveredSlice.sampleID) as label}
-                    <p class="text-sm">{label}</p>
+                    <p class="text-sm truncate">{label}</p>
                 {/each}
             {/if}
         </div>
     </div>
 
-    <div class="flex flex-col w-[12rem] ml-[2rem] truncate">
+    <div class="flex flex-col w-[12rem] ml-[2rem]">
         <p class="font-bold text-sm">Classification</p>
         {#if sampleManager.hoveredSlice != null}
-            <p class="text-sm">
+            <p class="text-sm truncate">
                 {sampleManager.hoveredSlice.viewTaxon.taxon.name}
             </p>
         {:else}
